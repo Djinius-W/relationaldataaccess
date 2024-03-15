8f25dccb3ded63868d80155e799234db6cb37939
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2024-03-15 11:52:22 +0100

    chore: add project files

---
+++ src/test/java/ch/etmles/fwbe/relationaldataaccess/RelationalDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.etmles.fwbe.relationaldataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
